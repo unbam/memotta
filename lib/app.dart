22945317f2dcea90fbb1f67fc726c16965ff407d
masashi takeshita <masashibox@gmail.com>
2020-10-14 18:52:20 +0900

    Cloud Functionsとの連携

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'blocs/flavor_provider.dart';
@@ -8,10 +10,24 @@ class App extends StatelessWidget {
     final flavor = FlavorProvider.of(context);
     print(flavor.toString());
 
+    // 非同期実行
+    method();
+
     return MaterialApp(
       home: Container(
         child: Text('Hello'),
       ),
     );
+  }
+
+  Future<void> method() async {
+    final cloudFunctions =
+        CloudFunctions(app: FirebaseApp.instance, region: 'asia-northeast1');
+
+    final callable =
+        cloudFunctions.getHttpsCallable(functionName: 'helloWorld2');
+
+    final result = await callable.call();
+    print(result.data.toString());
   }
 }
